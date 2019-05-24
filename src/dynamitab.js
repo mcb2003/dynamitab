08272edd8237142907c20ac0f3de9e4da2b807fa
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-24 21:44:05 +0100

    Tabview constructor now accepts optional arguments in an options object.

--- src/dynamitab.js
+++ src/dynamitab.js
@@ -2,11 +2,11 @@
 
 // this is a class definition for a Tab. Tabs can be added to TabView objects.
 class Tab {
-  constructor(tabview, id, title, description=undefined) {
+  constructor(tabview, id, title, description = undefined) {
     this._id = id;
     this.title = title;
     this.description = description;
-    
+
     this.class_prefix = tabview.class_prefix;
     this._tabview_id = tabview.id;
     this.panel_heading_level = tabview.panel_heading_level;
@@ -16,7 +16,7 @@ class Tab {
     this.panel_id = tabview.id + "-panel" + id;
     this.panel_class = tabview.class_prefix + "-panel";
   }
-  
+
   get id() {
     return this._id;
   }
@@ -25,7 +25,7 @@ class Tab {
     this.tab_id = this.tabview_id + "-tab" + value;
     this.panel_id = this.tabview_id + "-panel" + value;
   }
-  
+
   get tabview_id() {
     return this._tabview_id;
   }
@@ -35,8 +35,8 @@ class Tab {
     this.tab_class = value + "-tab";
     this.panel_id = value + "-panel" + this.id;
     this.panel_class = value + "-panel";
-  } 
-  
+  }
+
   get tab_element() {
     var tabobj = document.createElement("li");
     tabobj.setAttribute("role", "tab");
@@ -45,13 +45,13 @@ class Tab {
     tabobj.setAttribute("data-tabid", this.id);
     tabobj.setAttribute("tabindex", "0");
     tabobj.setAttribute("aria-controls", this.panel_id);
-    if(this.description != undefined) {
+    if (this.description != undefined) {
       tabobj.setAttribute("title", this.description);
     }
     tabobj.textContent = this.title;
     return tabobj;
   }
-  
+
   get panel_element() {
     var panelobj = document.createElement("section");
     panelobj.setAttribute("id", this.panel_id);
@@ -65,26 +65,26 @@ class Tab {
     panelobj.appendChild(panelheading);
     return panelobj;
   }
-  
+
   select() {
     var tab = document.querySelector("#" + this.tab_id);
-    if(tab && tab.getAttribute("aria-selected") != "true") {
+    if (tab && tab.getAttribute("aria-selected") != "true") {
       // get all tabs.
       var tabs = tab.parentElement.children;
       // deselect them all.
-      for(var i = 0; i < tabs.length; i++) {
+      for (var i = 0; i < tabs.length; i++) {
         tabs[i].setAttribute("aria-selected", "false");
       }
       // select the one that's clicked.
       tab.setAttribute("aria-selected", "true");
-      
+
       // get the current tab panel.
-      var panelid =tab.getAttribute("aria-controls"); 
+      var panelid = tab.getAttribute("aria-controls");
       var current_panel = document.querySelector("#" + panelid);
       // from this, get all the panels.
       var panels = current_panel.parentElement.children;
       // hide all panels.
-      for(var j = 0; j < panels.length; j++) {
+      for (var j = 0; j < panels.length; j++) {
         panels[j].setAttribute("aria-hidden", "true");
       }
       // show the correct panel.
@@ -95,11 +95,10 @@ class Tab {
 
     }
   }
-  
+
   get selected() {
     var tab = document.querySelector("#" + this.tab_id);
-    if(tab && tab.getAttribute("aria-selected") == "true") 
-{
+    if (tab && tab.getAttribute("aria-selected") == "true") {
       return true;
     } else {
       return false;
@@ -109,67 +108,73 @@ class Tab {
 
 // this is the class definition for TabView objects (the main object used when constructing a TabView).
 class TabView {
-  constructor(id, class_prefix=undefined, default_tab=1, expand_tabs=true, panel_heading_level=2, panel_heading_class=undefined) {
+  constructor(id, options = {
+    class_prefix: undefined,
+    default_tab: 1,
+    expand_tabs: true,
+    panel_heading_level: 2,
+    panel_heading_class: undefined
+  }) {
     this._id = id;
-    if(class_prefix === undefined) {
+    if (options.class_prefix === undefined) {
       this.class_prefix = id;
     } else {
-      this.class_prefix = class_prefix;
+      this.class_prefix = options.class_prefix;
     }
-    this.panel_heading_level = panel_heading_level;
-    if(panel_heading_class === undefined) {
+    this.panel_heading_level = options.panel_heading_level;
+    if (options.panel_heading_class === undefined) {
       this.panel_heading_class = this.class_prefix + "-panelheading";
     } else {
-      this.panel_heading_class = panel_heading_class;
+      this.panel_heading_class = options.panel_heading_class;
     }
     this.tabs = Array();
-    this.expand_tabs = expand_tabs;
-    this.default_tab = default_tab;
-    
+    this.expand_tabs = options.expand_tabs;
+    this.default_tab = options.default_tab;
+
     this.tablist_id = id + "-tablist";
     this.tablist_class = this.class_prefix + "-tablist";
   }
-  
+
   get id() {
     return this._id;
   }
   set id(value) {
     this._id = value;
-    if(this.tabs.length != 0) {
-      for(var i = 0; i < this.tabs.length; i++) {
+    if (this.tabs.length != 0) {
+      for (var i = 0; i < this.tabs.length; i++) {
         this.tabs[i].tabview_id = value;
       }
     }
   }
-   
+
   get tablist() {
     var tablist = document.createElement("ul");
     tablist.setAttribute("role", "tablist");
-   tablist.setAttribute("id", this.tablist_id);
+    tablist.setAttribute("id", this.tablist_id);
     tablist.setAttribute("class", this.tablist_class);
-    for(var i = 0; i < this.tabs.length; i++) {
+    for (var i = 0; i < this.tabs.length; i++) {
       var child = this.tabs[i].tab_element;
-      if(child.getAttribute("data-tabid") == this.default_tab) {
+      if (child.getAttribute("data-tabid") == this.default_tab) {
         child.setAttribute("aria-selected", "true");
       } else {
         child.setAttribute("aria-selected", "false");
       }
-      if(this.expand_tabs) {
+      if (this.expand_tabs) {
         child.style.width = "calc(98% / " + this.tabs.length + ")";
       }
       tablist.appendChild(child);
     }
     return tablist
   }
-  
+
   get tabview() {
     var tabview = document.createElement("section");
     tabview.setAttribute("id", this.id);
     tabview.appendChild(this.tablist);
     var tabpanel_container = document.createElement("div");
-    for(var i = 0; i < this.tabs.length; i++) {
-      var panel =this.tabs[i].panel_element;
-      if(panel.getAttribute("data-tabid") == this.default_tab) {
+    for (var i = 0; i < this.tabs.length; i++) {
+      var panel = this.tabs[i].panel_element;
+      if (panel.getAttribute("data-tabid") == this.default_tab) {
         panel.setAttribute("aria-hidden", "false");
       } else {
         panel.setAttribute("aria-hidden", "true");
@@ -179,49 +184,49 @@ class TabView {
     tabview.appendChild(tabpanel_container);
     return tabview;
   }
-  
+
   getTabById(id) {
-    for(var i = 0; i < this.tabs.length; i++) {
-      if(this.tabs[i].id == id) {
+    for (var i = 0; i < this.tabs.length; i++) {
+      if (this.tabs[i].id == id) {
         return this.tabs[i];
       }
     }
     return undefined;
   }
-  
+
   registerEvents() {
     var tab_elements = document.querySelector("#" + this.tablist_id)
     tab_elements = tab_elements.children;
     var self = this;
-    for(var i = 0; i < tab_elements.length; i++) {
-      tab_elements[i].addEventListener("click", function() {
+    for (var i = 0; i < tab_elements.length; i++) {
+      tab_elements[i].addEventListener("click", function () {
         var id = this.getAttribute("data-tabid");
-        if(id) {
+        if (id) {
           var tab = self.getTabById(id);
           tab.select();
         }
       });
-      tab_elements[i].addEventListener("keydown", function(event) {
-        if(event.which === 13) {
+      tab_elements[i].addEventListener("keydown", function (event) {
+        if (event.which === 13) {
           event.preventDefault();
           this.click();
-        } else if(event.which === 37) {
-        event.preventDefault();
-        self.previousTab();
-        } else if(event.which === 39) {
-        event.preventDefault();
-        self.nextTab();
+        } else if (event.which === 37) {
+          event.preventDefault();
+          self.previousTab();
+        } else if (event.which === 39) {
+          event.preventDefault();
+          self.nextTab();
         }
       });
     }
   }
-  
-  
+
+
   previousTab() {
     // get the index of the current tab.
     var index = this.tabs.indexOf(this.currentTab);
     // check if it's 0 and create a new_index variable depending on the result.
-    if(index === 0) {
+    if (index === 0) {
       var new_index = this.tabs.length - 1;
     } else {
       var new_index = index - 1;
@@ -230,12 +235,12 @@ class TabView {
     this.tabs[new_index].select();
 
   }
-  
+
   nextTab() {
     // get the index of the current tab.
     var index = this.tabs.indexOf(this.currentTab);
     // check if it's the last element and create a new_index variable depending on the result.
-    if(index === this.tabs.length - 1) {
+    if (index === this.tabs.length - 1) {
       var new_index = 0;
     } else {
       var new_index = index + 1;
@@ -244,10 +249,10 @@ class TabView {
     this.tabs[new_index].select();
 
   }
-  
+
   get currentTab() {
-    for(var i = 0; i < this.tabs.length; i++) {
-      if(this.tabs[i].selected === true) {
+    for (var i = 0; i < this.tabs.length; i++) {
+      if (this.tabs[i].selected === true) {
         return this.tabs[i];
       }
     }
